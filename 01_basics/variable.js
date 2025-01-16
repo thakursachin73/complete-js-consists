0b9cd6e9c1ecbb9ea97c7c54a9ebce233e88afb9
Sachin Singh <122783865+thakursachin73@users.noreply.github.com>
2025-01-16 05:19:34 +0000

    config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 123456;
+let accountEmail = "sachinsingh@gmaol.com";
+var accountPassword = 123454;
+accountCity = "Jaipur";
+
+// accountId = 2   - not allowed
+console.log(accountId);
+
+accountCity = "Bengluru";
+accountEmail = "hcb@gmail.com";
+accountPassword = 123123;
+
+
+console.table([accountEmail, accountPassword, accountCity]);
+
+
+
